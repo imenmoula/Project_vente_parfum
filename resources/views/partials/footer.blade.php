8623d682dad5f259bff5b2f74e9e8aad4f2183b8
moulaimen <votre_email>
2024-05-19 18:01:13 +0200

    fix : fix design dashbord

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="footer">
+    <div class="copyright">
+        <p> Copyright © 2024 Moula Imen - parfum. Tous droits réservés</p>
+
+    </div>
+</div>	
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,54 @@
+<div class="deznav">
+    <div class="deznav-scroll">
+        <ul class="metismenu" id="menu">
+
+            <li><a href="" class="ai-icon" aria-expanded="false">
+
+                    <i class="flaticon-381-television"></i>
+                    <span class="nav-text">Categories</span>
+                </a>
+            </li>
+
+            <li><a href="" class="ai-icon" aria-expanded="false">
+                    <i class="flaticon-381-settings-2"></i>
+                    <span class="nav-text">Plats</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="" class="ai-icon" aria-expanded="false">
+                    
+                    <span class="nav-text">Commande</span>
+                </a>
+            </li>
+
+
+            <li>
+                <a href="" class="ai-icon" aria-expanded="false">
+                    
+                    <span class="nav-text">Utilisateur</span>
+                </a>
+            </li>
+
+            
+
+            <li>
+                <a href="" class="ai-icon" aria-expanded="false">
+                    
+                    <span class="nav-text">Reservation</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="" class="ai-icon" aria-expanded="false">
+                    
+                    <span class="nav-text">Contact</span>
+                </a>
+            </li>
+            
+        </ul>
+
+
+
+    </div>
+</div>
